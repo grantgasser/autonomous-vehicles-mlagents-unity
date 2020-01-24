67a5bc0a7d76228437e2986457b8cb5b070b3311
blainerothrock <blaine.p.rothrock@gmail.com>
2020-01-24 08:52:26 -0600

    ml-agents demo project

---
+++ exploration/mlagents/RollerBall/Assets/Scripts/RollerAgent.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using MLAgents;
+
+public class RollerAgent : Agent
+{
+
+    public Rigidbody rBody;
+    public Transform Target;
+    public float directionAngle;
+
+    public float speed = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rBody = GetComponent<Rigidbody>();
+        Debug.Log("Starting RollerAgent");
+        Monitor.SetActive(true);
+    }
+
+    public override void CollectObservations()
+    {
+        // Target and Agent Positions
+        AddVectorObs(Target.localPosition);
+        AddVectorObs(this.transform.localPosition);
+
+        // Agent velocity
+        AddVectorObs(rBody.velocity.x);
+        AddVectorObs(rBody.velocity.z);
+    }
+
+    public override void AgentAction(float[] vectorAction)
+    {
+        // Action Size  = 2  m ,
+        Vector3 controlSignal = Vector3.zero;
+        controlSignal.x = vectorAction[0];
+        controlSignal.z = vectorAction[1];
+        rBody.AddForce(controlSignal * speed);
+        directionAngle = Mathf.Atan2(rBody.velocity.y, rBody.velocity.x) * Mathf.Rad2Deg;
+
+        // Rewards
+        float distanceToTarget = Vector3.Distance(
+            this.transform.localPosition,
+            Target.localPosition
+        );
+
+        // Reached Target
+        if (distanceToTarget < 1.42f)
+        {
+            SetReward(1.0f);
+            Done();
+        }
+
+        // Fell off platform
+        if (this.transform.localPosition.y < 0)
+        {
+            Done();
+        }
+    }
+
+    public override void AgentReset()
+    {
+        if (this.transform.localPosition.y < 0)
+        {
+            // If the agent fell, zero its momentum
+            this.rBody.angularVelocity = Vector3.zero;
+            this.rBody.velocity = Vector3.zero;
+            this.transform.localPosition = new Vector3(0, 0.5f, 0);
+        }
+
+        // Move the target to a new spot
+        Target.localPosition = new Vector3(
+            Random.value * 21f - 10.5f,
+            0.5f,
+            Random.value * 21f - 10.5f
+        );
+    }
+
+    public override float[] Heuristic()
+    {
+        var action = new float[2];
+
+        action[0] = Input.GetAxis("Horizontal");
+        action[1] = Input.GetAxis("Vertical");
+        return action;
+    }
+
+}
---
+++ exploration/mlagents/RollerBall/Assets/Scripts/RollerCameraController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class RollerCameraController : MonoBehaviour
+{
+
+    public RollerAgent roller;
+    public float imgFrequency = 0.5f;
+    public bool record = false;
+
+    private Vector3 offset;
+
+    private int FileCounter = 0;
+    private float time = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - roller.transform.position;
+    }
+
+    private void Update()
+    {
+        
+
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = transform.position = roller.transform.position + offset;
+
+        transform.rotation = Quaternion.LookRotation(roller.rBody.velocity);
+        time += Time.deltaTime;
+        if ( record && time >= imgFrequency )
+        {
+            time = 0.0f;
+            CamCapture();
+        }
+    }
+
+    void CamCapture()
+    {
+        Camera Cam = GetComponent<Camera>();
+
+        RenderTexture currentRT = RenderTexture.active;
+        RenderTexture.active = Cam.targetTexture;
+
+        Cam.Render();
+
+        Texture2D Image = new Texture2D(Cam.targetTexture.width, Cam.targetTexture.height);
+        Image.ReadPixels(new Rect(0, 0, Cam.targetTexture.width, Cam.targetTexture.height), 0, 0);
+        Image.Apply();
+        RenderTexture.active = currentRT;
+
+        var Bytes = Image.EncodeToPNG();
+        Destroy(Image);
+
+        string path = Application.dataPath + "/Camera/" + roller.GetInstanceID() + "_" + FileCounter + ".png";
+        File.WriteAllBytes(path, Bytes);
+        FileCounter++;
+    }
+}
