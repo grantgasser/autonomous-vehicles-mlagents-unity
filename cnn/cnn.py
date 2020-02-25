dab21d29770392212434665aff3154ce6afa8f18
Grant Gasser <glgasser@gmail.com>
2020-02-24 22:08:55 -0600

    Being constructing model and training/eval implementation.

--- cnn/cnn.py
+++ cnn/cnn.py
@@ -58,19 +58,42 @@ def main():
     # normalize between 0-1
     x_train, x_test = x_train / 255.0, x_test / 255.0
 
-    # create model: https://www.tensorflow.org/tutorials/images/cnn
+    # create model
+    # ---------------
+    # output size = W (input) - K (filter) + 1 = 256 - 3 + 1 = 254
+    model = models.Sequential()
+    model.add(layers.Conv2D(x_train.shape[1], (3,3), activation='relu', input_shape=(x_train.shape[1:])))
+    model.add(layers.MaxPooling2D((2, 2)))
+    model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+    model.add(layers.MaxPooling2D((2, 2)))
+    model.add(layers.Flatten())
+    model.add(layers.Dense(64, activation='relu'))
+    model.add(layers.Dense(1))
+
+    # review model
+    print(model.summary())
     # ---------------
 
-
+    # compile train model
     # ---------------
-
-    # train model
-    # ---------------
-
+    # model.compile(optimizer='adam',
+    #               loss=tf.keras.losses.MeanSquaredError,
+    #               metrics=['MeanSquaredError'])
+    #
+    # history = model.fit(x_train, y_train, epochs=5, validation_data=(x_test, y_test))
     # ---------------
 
     # evaluate model
     # ---------------
+    # plt.plot(history.history['accuracy'], label='accuracy')
+    # plt.plot(history.history['val_accuracy'], label='val_accuracy')
+    # plt.xlabel('Epoch')
+    # plt.ylabel('Accuracy')
+    # plt.ylim([0.5, 1])
+    # plt.legend(loc='lower right')
+    #
+    # test_loss, test_acc = model.evaluate(x_test, y_test, verbose=2)
+    # print('\nTest Accuracy:', test_acc)
 
     # ---------------
 
